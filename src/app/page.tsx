c2f42b3ee1800999fe9df4fafca78b4ba0f51cc5
therealcos <therealcos@therealcoss-mbp.lan>
2024-12-19 23:29:00 -1000

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ import {
 import { pitAbi, tableAbi } from "../abi";
 import { useEffect, useState } from "react";
 import { ContractFunctionParameters } from "viem";
+import Modal from "./ui/Modal";
 
 function App() {
   const account = useAccount();
@@ -19,9 +20,8 @@ function App() {
   const { writeContract } = useWriteContract();
   const { disconnect } = useDisconnect();
   const address = "0xff844a27C2C80649Cb76d56f9A36c3Cd274Dfb49";
-  const [tableReads, setTableReads] = useState<ContractFunctionParameters[]>(
-    []
-  );
+  const [tableReads, setTableReads] = useState<ContractFunctionParameters[]>();
+  const [showCreateTable, setShowCreateTable] = useState(false);
 
   // useWatchContractEvent({
   //   address,
@@ -30,6 +30,39 @@ function App() {
   //   onLogs: (logs) => console.log("Table Created", logs),
   //   onError: (error) => console.log("Error", error)
   // })
+
+  // writeContract(
+  //   {
+  //     abi: pitAbi,
+  //     address,
+  //     functionName: "createTable",
+  //     args: [
+  //       7, // _maxPlayers
+  //       [
+  //         // _betRange
+  //         0, // min
+  //         100, // max
+  //       ],
+  //       [
+  //         // _rules
+  //         2, // deckCount
+  //         false, // dealerHitOnSoft17
+  //         false, // allowDoubleAfterSplit
+  //         1, // doubleRule
+  //         3, // maxResplitHands
+  //         true, // allowResplitAces
+  //         true, // allowHitSplitAces
+  //         true, // allowLateSurrender
+  //         true, // allowInsurance
+  //         false, // sixToFive
+  //       ],
+  //       "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d", // _token
+  //     ],
+  //   },
+  //   {
+  //     onError: (e) => console.log(e),
+  //   }
+  // );
 
   const { data: tables, error: err } = useReadContract({
     address,
@@ -56,102 +89,77 @@ function App() {
   const { data: tableInfo } = useReadContracts({ contracts: tableReads });
 
   return (
-    <div>
+    <>
       <div>
-        <h2>Account</h2>
-
         <div>
-          status: {account.status}
-          <br />
-          addresses: {JSON.stringify(account.addresses)}
-          <br />
-          chainId: {account.chainId}
+          <h2>Account</h2>
+
+          <div>
+            status: {account.status}
+            <br />
+            addresses: {JSON.stringify(account.addresses)}
+            <br />
+            chainId: {account.chainId}
+          </div>
+
+          {account.status === "connected" && (
+            <>
+              <button type="button" onClick={() => disconnect()}>
+                Disconnect
+              </button>
+              <button
+                type="button"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4"
+                onClick={() => {
+                  console.log("HEY");
+                  setShowCreateTable(true);
+                }}
+              >
+                Create Table
+              </button>
+            </>
+          )}
         </div>
 
-        {account.status === "connected" && (
-          <>
-            <button type="button" onClick={() => disconnect()}>
-              Disconnect
+        <div>
+          <h2>Connect</h2>
+          {connectors.map((connector) => (
+            <button key={connector.uid} onClick={() => connect({ connector })}>
+              {connector.name}
             </button>
-            <button
-              type="button"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4"
-              onClick={async () => {
-                // TODO: show modal with all options
-
-                writeContract(
-                  {
-                    abi: pitAbi,
-                    address,
-                    functionName: "createTable",
-                    args: [
-                      7, // _maxPlayers
-                      [
-                        // _betRange
-                        0, // min
-                        100, // max
-                      ],
-                      [
-                        // _rules
-                        2, // deckCount
-                        false, // dealerHitOnSoft17
-                        false, // allowDoubleAfterSplit
-                        1, // doubleRule
-                        3, // maxResplitHands
-                        true, // allowResplitAces
-                        true, // allowHitSplitAces
-                        true, // allowLateSurrender
-                        true, // allowInsurance
-                        false, // sixToFive
-                      ],
-                      "0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d", // _token
-                    ],
-                  },
-                  {
-                    onError: (e) => console.log(e),
-                  }
-                );
-              }}
-            >
-              Create Table
-            </button>
-          </>
-        )}
+          ))}
+          <div>{status}</div>
+          <div>{error?.message}</div>
+        </div>
+        <div>
+          <h1>Tables</h1>
+          {tableInfo?.map((table, index) => {
+            const info: any = table.result;
+            return (
+              <a
+                className="flex flex-col max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 gap-2"
+                key={`table-card-${index}`}
+                href="#"
+              >
+                <div>
+                  <div className="b">Token</div>
+                  {info[0]}
+                </div>
+                <div>
+                  <h3>Players</h3>
+                  {info[1]}
+                </div>
+              </a>
+            );
+          })}
+        </div>
       </div>
-
-      <div>
-        <h2>Connect</h2>
-        {connectors.map((connector) => (
-          <button key={connector.uid} onClick={() => connect({ connector })}>
-            {connector.name}
-          </button>
-        ))}
-        <div>{status}</div>
-        <div>{error?.message}</div>
-      </div>
-      <div>
-        <h1>Tables</h1>
-        {tableInfo?.map((table, index) => {
-          const info: any = table.result;
-          return (
-            <a
-              className="flex flex-col max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 gap-2"
-              key={`table-card-${index}`}
-              href="#"
-            >
-              <div>
-                <div className="b">Token</div>
-                {info[0]}
-              </div>
-              <div>
-                <h3>Players</h3>
-                {info[1]}
-              </div>
-            </a>
-          );
-        })}
-      </div>
-    </div>
+      {showCreateTable && (
+        <Modal>
+          <div>HELLO</div>
+        </Modal>
+      )}
+    </>
   );
 }
 
